c3f32e506e9c13fa50b7c310ab1d122cf7f104cd
sethwboss <sethwboss@gmail.com>
2026-04-23 18:07:40 -0600

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+        string grade = Console.ReadLine();
+
+        int gradeint = int.Parse(grade);
+
+        string letter = "";
+        if (gradeint >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeint >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeint >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeint >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade of {gradeint} gets you a letter grade of {letter}");
+
+        if (gradeint > 69)
+        {
+            Console.WriteLine("Congratulations on passing the course!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, your grade did not pass the class. Better luck next time!");
+        }
     }
 }
