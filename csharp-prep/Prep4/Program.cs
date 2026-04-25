ecfa1465637deaa26d427bf0e4d4549684cee4b2
sethwboss <sethwboss@gmail.com>
2026-04-25 10:44:17 -0600

    finished prep 4 & 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+
+        List<int> numbers = new List<int>();
+
+        int currentnum = 1;
+        int testnum = 0;
+        Boolean loop = true;
+        while (loop == true)
+        {
+            Console.WriteLine("Enter number: ");
+            currentnum = int.Parse(Console.ReadLine());
+
+    
+
+            if (currentnum == 0)
+            {
+                loop = false;
+            }
+            else
+            {
+                numbers.Add(currentnum);
+                testnum = currentnum;
+            }
+
+        }
+
+        int sum = 0;
+        int largestnum = testnum;
+        int count = 0;
+        foreach (int number in numbers)
+        {
+            count ++;
+            currentnum = number;
+            sum += currentnum;
+            
+            if (currentnum > largestnum)
+            {
+                largestnum = currentnum;
+            }
+            else {}
+        }
+
+        double average = ((double)sum / count);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestnum}");
+
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,61 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+        {
+            DisplayWelcome();
+            string name = PromptUserName();
+            double userNumber = PromptUserNumber();
+            int userBirthYear = PromptUserBirthYear();
+            double numSquared = SquareNumber(userNumber);
+            DisplayResult(name, numSquared, userBirthYear);
+        }
+
+
+
+
+    static void DisplayWelcome()
+        {
+        Console.WriteLine("Welcome to the program!");
+        }
+
+    static string PromptUserName()
+        {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+        }
+
+    static double PromptUserNumber()
+        {
+        Console.Write("Please enter your favorite number: ");
+        double userNumber = double.Parse(Console.ReadLine());
+        return userNumber;
+        }
+
+    static int PromptUserBirthYear()
+        {
+        Console.Write("Please enter the year you were born: ");
+        int userBirthYear = int.Parse(Console.ReadLine());
+        return userBirthYear;
+        }
+
+    static double SquareNumber(double number)
+        {
+            double numberSquared = (number * number);
+            return numberSquared;
+        }
+
+    static void DisplayResult(string name, double numSquared, int birthYear)
+        {
+            int year = DateTime.Now.Year;
+            int turnAge = (year - birthYear);
+
+            Console.WriteLine($"{name}, the square of your number is {numSquared}.");
+            Console.WriteLine($"{name}, you will turn {turnAge} this year.");
+
+        }
+
+
 }
