390feb9128bff23ed4c90ea7034dfcdafc71767a
sethwboss <sethwboss@gmail.com>
2026-04-25 08:47:52 -0600

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+
+        int guessnum = 101;
+        while (guessnum != number)
+        {
+            
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessnum = int.Parse(guess);
+
+            if (number > guessnum) {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guessnum) {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
