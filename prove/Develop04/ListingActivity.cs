d873d7d170d86cb1047e1be814cd274dae2cda2c
sethwboss <sethwboss@gmail.com>
2026-06-10 15:21:11 -0600

    more progress on reflectyionacitivity and listingactivity

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -2,4 +2,38 @@ public class ListingActivity : Activity
 {
     public ListingActivity() : base("Listing Activity", "reflect on the good things in your life by having you list as many things as you can in a certain area.") {}
 
+    private  List<string> _prompts = new List<string>("When have you felt the Holy Ghost this month?");
+
+
+    public void RunActivity()
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            
+        }
+    }
+
+    private string GetPrompt()
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(_prompts.Count);
+        string _prompt = _prompts[randomIndex];
+        return _prompt;
+    }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine();
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine();
+
+        string prompt = GetPrompt();
+        Console.WriteLine($" --- {prompt} ---");
+        Console.Write("You may begin in: ");
+        LoadingTimer(5);
+    }
+
 }
--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -3,7 +3,7 @@ public class ReflectionActivity : Activity
         public ReflectionActivity() : base("Reflecting Activity", "reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.") {}
 
     private  List<string> _prompts = new List<string>("Think of a time when you did something really difficult.");
-    private  List<string> questions = new List<string>("How did you feel when it was complete?", "What is your favorite thing about this experience?");
+    private  List<string> _questions = new List<string>("How did you feel when it was complete?", "What is your favorite thing about this experience?");
 
 
 
@@ -19,25 +19,58 @@ public class ReflectionActivity : Activity
         }
     }
 
+    private string GetPrompt()
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(_prompts.Count);
+        string _prompt = _prompts[randomIndex];
+        return _prompt;
+    }
+    private string GetQuestion()
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(_questions.Count);
+        string _questions = _questions[randomIndex];
+        return _questions;
+    }
 
-    private void DisplayPrompt()
+    public void DisplayPrompt()
     {
         Console.WriteLine();
         Console.WriteLine("Consider the following prompt:");
         Console.WriteLine();
 
+        string prompt = GetPrompt();
 
-        Console.WriteLine($" --- {} ---")
+        Console.WriteLine($" --- {prompt} ---");
 
         Console.WriteLine();
         Console.Write("When you have something in mind, press enter to continue.");
         Console.ReadLine();
     }
 
-    private void DisplayQuestions()
+    public void DisplayQuestions()
     {
         Console.WriteLine();
         Console.WriteLine("Now ponder on each of the following questions as they relate to this experience");
+        Console.Write("You may begin in: ");
+        LoadingTimer(5);
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            string question = GetQuestion();
+            Console.WriteLine($"> {question}");
+            LoadingSpinner(5);
+
+            // string answer = Console.ReadLine();
+            // append answer to list of answer and save in separate file
+
+            //could also make sure no duplicate question until we've gone through them all  - frmo the video
+
+        }
     }
 
 }
